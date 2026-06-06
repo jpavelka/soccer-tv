e8bdb10b7e52a047717b7dd560a02b5f6ebc7353
jeffp171 <jeffp171@gmail.com>
2026-06-06 20:23:13 +0000

    minor

--- src/routes/Modal.svelte
+++ src/routes/Modal.svelte
@@ -39,6 +39,7 @@
         left: 0;
         width: 100%;
         height: 100%;
+        z-index: 1000;
         background-color: rgba(0, 0, 0, 0.5);
         /* display: flex;
         justify-content: center;
