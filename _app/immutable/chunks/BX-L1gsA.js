698dfc4e984980d295f4f7274bf0fa56ddd915da
jpavelka <jpavelka@users.noreply.github.com>
2026-07-09 02:38:02 +0000

    Deploying to gh-pages from @ jpavelka/soccer-tv@928656f1c8ce671f72a73f00e8e450558f00d03c 🚀

--- _app/immutable/chunks/BX-L1gsA.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1w94o9l?.base??"/soccer-tv",e=globalThis.__sveltekit_1w94o9l?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/entry/start.DHaUQiyN.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BDaEnCJy.js";export{o as load_css,r as start};
